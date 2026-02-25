6a36e31dd5a7006313820a22f1f02ca87bcf6491
ninom <ninom@cs.washington.edu>
2026-02-24 18:35:17 -0800

    changed collab code to python code

---
+++ code/identify_subreddits.py
@@ -0,0 +1,76 @@
+from datetime import datetime, timedelta
+import itertools
+import json
+from langdetect import detect, DetectorFactory
+import logging
+import pandas
+import os
+import requests
+import sys
+import time
+from tqdm import tqdm
+import util
+
+#### GLOBAL VARIABLES #### 
+CODE_NAME = "identify_subreddits"
+WORK_DIR = "../"
+QUERIES_FILE = os.path.join(WORK_DIR, "subreddit_queries.csv")
+SUBREDDITS_FILE = os.path.join(WORK_DIR, "subreddits.json")
+
+# PushPull URL
+BASE_URL = "https://api.pullpush.io/reddit/search/submission/"
+
+# PushPull Scraping Interval
+INTERVAL = 10 # Unit: days
+START = datetime(2022, 11, 30)
+END = datetime(2026, 2, 1)
+
+###### PROCESS REQUEST ######
+def process_request(data, params, logger):
+
+   # Count how many of the potential posts are valid
+   num_valid_data = 0
+   for post in data:
+      title = post.get('title', '')
+      body = post.get('selftext', '')
+      
+      if util.is_english(post.get('title', '')) and len(body) > 5:
+         num_valid_data += 1
+         subreddit = post.get('subreddit')
+         
+         with open(SUBREDDITS_FILE, 'r') as file:
+            subreddits = json.load(file)
+            subreddits[subreddit] = subreddits.get(subreddit, 0) + 1
+
+            with open(SUBREDDITS_FILE, 'w', encoding='utf-8') as f:
+               json.dump(subreddits, f, indent=4)
+   
+   logger.info(f"Validating Term: {params['title']}: Found {num_valid_data} valid match(es).")
+   return num_valid_data
+
+###### RUN QUERIES ######
+def run_queries(queries, logger):
+    for indx, row in tqdm(queries.iterrows(), total=queries.shape[0]):
+        if row["count"] == -1:
+            row = util.run_query(row, BASE_URL, process_request, logger)
+            queries.at[indx, "count"] = row["count"]
+            queries.to_csv(QUERIES_FILE)
+            time.sleep(4)
+    return queries
+
+if __name__ == "__main__":
+    # Read the config file. This contains search keywords.
+    config = util.read_config(f'{WORK_DIR}/config.json')
+    # Create a logger for our code.
+    now = datetime.now().strftime("%y%m%d%H%M")
+    logger = util.initialize_logger(log_file = f"{WORK_DIR}/logs/{now}_{CODE_NAME}_log.log")
+    
+    queries_kwargs = {
+       'config': config,
+       'start': START,
+       'end': END,
+       'interval': INTERVAL
+    }
+    queries = util.generate_queries(queries_file = QUERIES_FILE, **queries_kwargs)
+    
+    queries = run_queries(queries, logger)
---
+++ code/util.py
@@ -0,0 +1,157 @@
+from datetime import datetime, timedelta
+import itertools
+import json
+from langdetect import detect, DetectorFactory
+import logging
+import pandas
+import os
+import requests
+import sys
+import time
+from tqdm import tqdm
+import traceback
+
+###### Read Config Logger ######
+def read_config(config_file):
+    with open(config_file, 'r') as file:
+        config = json.load(file)
+        return config
+
+###### Initialize Logger ######
+def initialize_logger(log_file = "./logs/log.log"):
+    """
+    Initializes the logger for our code. 
+
+    Parameters:
+    log_file: The path for the file that will contain our code logs. Default: ./logs/log.log
+    """
+    # 1. Create the logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # 2. Create the format
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+
+    # 3. File Handler
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+
+    # 4. Stream Console Handler
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+
+    # 5. Add handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+###### FORMAT ISODATE ######
+def format_isodate_to_date(dt):
+   dt_obj = datetime.fromisoformat(dt).date()
+   df_string = dt_obj.strftime('%Y_%m_%d')
+   return df_string
+
+###### IS ENGLISH ######
+def is_english(text):
+    try:
+        return detect(text) == 'en'
+    except:
+        return False
+
+###### Generate Queries Dataset ######  
+def _generate_queries_all(**kwargs):
+  """
+  Creates dataset of all the queries that we have to run by finding the cross-product of all possible
+  AI-related keywords, environmental keywords, and dates.
+  """
+  config = kwargs.get("config", {}) #read_config("../config.json"))
+  start = kwargs.get("start", datetime(2022, 11, 30))
+  end = kwargs.get("end", datetime(2026, 2, 1))
+  interval = kwargs.get("interval", 10)
+
+  ai_terms = config["keywords"]["ai"]
+  env_terms = config["keywords"]["env"]
+  
+  # create dates from the ```start''' date until the  ```end''' date, with ```interval''' days in between
+  date_range = pandas.date_range(start, end, freq=f"{interval}D")
+
+  queries = pandas.DataFrame(
+      list(itertools.product(ai_terms, env_terms, date_range)), 
+      columns = ["ai", "env", "start"])
+  queries["end"] = queries["start"] + timedelta(interval)
+
+  return queries
+
+###### Generate Queries Already Completed ###### 
+def _generate_queries_already_run(queries_file = "./queries_run.csv", **kwargs):
+  """
+  Returns dataset of queries already run. Queries that have already been run are within a .csv file.
+  Otherwise returns an empty dataframe.
+  """
+  
+  if os.path.isfile(queries_file):
+    queries_run = pandas.read_csv(queries_file)
+    queries_run = queries_run.drop("Unnamed: 0", axis = 1)
+    queries_run["start"] = pandas.to_datetime(queries_run["start"])
+    queries_run["end"] = pandas.to_datetime(queries_run["end"])
+    return queries_run
+
+  return pandas.DataFrame([], columns = ["ai", "env", "start", "end", "count"])
+
+###### Generate Queries Dataset ###### 
+def generate_queries(queries_file = "./queries_run.csv", **kwargs):
+   """
+   Returns dataset of all the queries that have been run, and still need to be run. Those that have been
+   already run will contain a non-negative entry in the "count" column. Those that still been to be processed
+   will contain a -1 value in the "count" column.
+   """
+   
+   queries_all = _generate_queries_all(**kwargs)
+   queries_run = _generate_queries_already_run(queries_file, **kwargs)
+   queries_all = queries_all.merge(queries_run,
+                                     how = "left",
+                                     on = ["ai", "env", "start", "end"])
+   queries_all = queries_all.infer_objects(copy=False).fillna(-1)
+   
+   return queries_all
+
+def send_request(params, base_url, process_request, logger):
+   try:
+    response = requests.get(base_url, params=params)
+    response.raise_for_status()
+    
+    data = response.json().get('data', [])
+    logger.info(f"Processing Term: {params['title']}: Found {len(data)} potential matches.")
+    
+    return process_request(data, params, logger)
+   
+   except Exception as e:
+    logger.info(f"Error on {params['title']}: {e}")
+    raise ValueError()
+
+###### RUN ONE QUERY ######
+def run_query(row, base_url, process_request, logger):
+   after = int(row["start"].timestamp())
+   before = int(row["end"].timestamp())
+   logger.info(f"Starting scrape from {row['start'].date()} to {(row['end']).date()}...")
+   
+   params = {
+     # include this tag if we want to only search in the title of the post
+     'title': f"{row['ai']} {row['env']}",
+     # include this tag if we want to only search in both title and body of the post
+     # 'both': f"{row['ai']} {row['env']}",
+     'after': after,
+     'before': before,
+     'size': 100,
+     'sort': 'asc'
+     }
+   
+   try:
+    results = send_request(params, base_url, process_request, logger)
+    row["count"] = results
+   
+   except ValueError as e:
+      print(traceback.format_exc())
+
+   return row
